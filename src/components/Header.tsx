793739d7df6c3f00952f3c531f968ee267566622
Sandroo10 <sandrosaralidze15@gmail.com>
2025-08-08 17:48:12 +0400

    smol change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,8 +18,8 @@ const Header = () => {
   const navItems = [
     { name: 'Home', href: '#home' },
     { name: 'About', href: '#about' },
-    { name: 'Skills', href: '#skills' },
     { name: 'Projects', href: '#projects' },
+    { name: 'Skills', href: '#skills' },
     { name: 'Contact', href: '#contact' }
   ];
 
